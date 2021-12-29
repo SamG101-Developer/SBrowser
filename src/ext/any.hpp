bb46a250062f1321af9003606c67d0597d54e286
Sam <samuelgardner101@gmail.com>
2021-12-29 09:52:08 +0000

    Added a new project to Git

--- src/ext/any.hpp
+++ src/ext/any.hpp
@@ -4,6 +4,7 @@
 #include <any>
 
 #include <ext/decorators.hpp>
+#include <ext/string.hpp>
 
 
 //inline bool operator< (const std::any& first, const std::any& second) {return &first <  &second;}
@@ -12,20 +13,8 @@
 
 
 namespace ext {
-    template <class T> T any_cast(std::any value);
     class any;
-}
-
-
-template <class T>
-T ext::any_cast(std::any value) {
-    if (not value.has_value() or value.type() == typeid(value))
-        return T(); // std::is_pointer_v<T> ? nullptr : T(); -> if constexpr () {}
-    try {
-        return std::any_cast<T>(value);
-    } catch(...) {
-        return std::any_cast<T>(std::any());
-    }
+    template <class T> T any_cast(ext::any value);
 }
 
 
@@ -39,8 +28,13 @@ public: constructors
 
     explicit any(const std::any& other) : m_value(other) {};
     explicit any(std::any&& other) noexcept : m_value(other) {};
+    ext::any& operator=(const std::any& other) {m_value = other; return *this;}
+    ext::any& operator=(std::any&& other) noexcept {m_value = other; return *this;}
 
-    operator std::any() const {return m_value;}
+public: // TODO : annotation
+    operator std::any() const {
+        return m_value;
+    }
 
     template <typename T>
     explicit operator T() const requires (not std::is_same_v<T, std::any>) {
@@ -48,12 +42,38 @@ public: constructors
     }
 
 public: methods
-    const type_info& type() const {return m_value.type();}
-    const bool empty() const {return not m_value.has_value();}
+    const type_info& type() const {
+        return m_value.type();
+    }
+
+    bool empty() const {
+        return not m_value.has_value();
+    }
+
+    bool contains_pointer() const {
+        return ext::string{type().name()}.contains(*"* __ptr64");
+    }
+
+    template <typename T>
+    void emplace(T&& element) {
+        m_value.template emplace<T>(element);
+    }
 
 private: internal_properties
     std::any m_value;
 };
+
+
+template <class T>
+T ext::any_cast(ext::any value) {
+    if (value.empty() or value.type() == typeid(value))
+        return value.contains_pointer() ? nullptr : T();
+    try {
+        return std::any_cast<T>(value);
+    } catch(...) {
+        return std::any_cast<T>(std::any());
+    }
+}
 
 
 #endif //SBROWSER_ANY_HPP
