6b5b3667e7058429a3a1b6b3e70d9e2b0116f2f4
Sam <samuelgardner101@gmail.com>
2022-05-07 13:15:30 +0100

    Created custom impossible exceptions

--- src/ext/exception.hpp
+++ src/ext/exception.hpp
@@ -3,7 +3,16 @@
 
 #include <exception>
 
-#define INVALID_ENUM(argument) std::runtime_error{"Invalid enum value (impossible)"}
-#define INVALID_SWITCH(argument) std::runtime_error{"Invalid switch value (impossible)"}
+namespace ext {class impossible_exception;}
+namespace ext {class invalid_enum_enumerator_exception;}
+namespace ext {class invalid_switch_case_exception;}
+
+
+// base class for impossible exceptions
+class ext::impossible_exception : public std::runtime_error {};
+
+// common exceptions thrown in situations that shouldn't ever happen
+class ext::invalid_enum_enumerator_exception : public impossible_exception {};
+class ext::invalid_switch_case_exception : public impossible_exception {};
 
 #endif //SBROWSER_EXCEPTION_HPP
