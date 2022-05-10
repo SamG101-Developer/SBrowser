dc4b6ada1f092d893ce5d90b9aeb065d114c6d55
Sam <samuelgardner101@gmail.com>
2022-05-10 10:25:46 +0100

    Changed custom exception class structures, more elegant and STL-like (although simplified)

--- src/ext/exception.hpp
+++ src/ext/exception.hpp
@@ -1,18 +1,47 @@
 #ifndef SBROWSER_EXCEPTION_HPP
 #define SBROWSER_EXCEPTION_HPP
 
-#include <exception>
+#include <stdexcept>
+#include <ext/string.hpp>
 
 namespace ext {class impossible_exception;}
 namespace ext {class invalid_enum_enumerator_exception;}
 namespace ext {class invalid_switch_case_exception;}
+namespace ext {class type_error;}
 
 
 // base class for impossible exceptions
-class ext::impossible_exception : public std::runtime_error {};
+class ext::impossible_exception : public std::runtime_error
+{
+public:
+    using _Mybase = std::runtime_error;
+    using std::runtime_error::runtime_error;
+};
+
 
 // common exceptions thrown in situations that shouldn't ever happen
-class ext::invalid_enum_enumerator_exception : public impossible_exception {};
-class ext::invalid_switch_case_exception : public impossible_exception {};
+class ext::invalid_enum_enumerator_exception : public impossible_exception
+{
+public:
+    using _Mybase = impossible_exception;
+    using impossible_exception::impossible_exception;
+};
+
+
+class ext::invalid_switch_case_exception : public impossible_exception
+{
+public:
+    using _Mybase = impossible_exception;
+    using impossible_exception::impossible_exception;
+};
+
+
+// common exceptions that should be customized
+class ext::type_error : public std::runtime_error
+{
+public:
+    using _Mybase = std::runtime_error;
+    using std::runtime_error::runtime_error;
+};
 
 #endif //SBROWSER_EXCEPTION_HPP
